eee8bb6b7bddbc9d6647f314bee5d9ada53ce77a
qicaisheng <qicaisheng@qq.com>
2015-04-20 21:08:53 +0800

    finish log in /log out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,5 +64,6 @@ describe User do
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
-  end    
+  end   
+
 end
